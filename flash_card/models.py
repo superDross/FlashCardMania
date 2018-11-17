e4129900385d3409999ea4030393f79930e9e4af
david <dross78375@gmail.com>
2018-11-17 18:43:15 +0000

    Added FlashCard model

--- flash_card/models.py
+++ flash_card/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class FlashCard(models.Model):
+    ''' Represents a single flash card.'''
+
+    LEVELS = tuple((x, x) for x in range(1, 101))
+
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.SET_NULL,
+                                   null=True,
+                                   blank=True)
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='image_files/')
+    audio = models.FileField(upload_to='audio_files/')
+    level = models.IntegerField(choices=LEVELS)
+
+    def __str__(self):
+        return self.pk
